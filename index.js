af1f10d2895cd80dab8ddb3f0a2b70fd6b4056ec
Mariana Usuga <marianausuga275004@outlook.es>
2021-06-01 20:31:10 -0500

    add method addeventlistener with the click event to play sound

--- index.js
+++ index.js
@@ -2,9 +2,40 @@ var numberButtons=document.querySelectorAll(".drum").length;
 
  for(var i=0; i<numberButtons; i++){
    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
-    var audio=new Audio('sounds/tom-'+i+'.mp3')
-    audio.play();
-    });
-        /*alert("I got clicked!")*/;
-
- }
+     var click=this.innerHTML;
+     var audioDrum;
+      switch(click){
+        case "w":
+          audioDrum=new Audio("sounds/tom-1.mp3");
+          audioDrum.play();
+          break;
+        case "a":
+          audioDrum=new Audio("sounds/tom-2.mp3");
+          audioDrum.play();
+          break;
+        case "s":
+          audioDrum=new Audio("sounds/tom-3.mp3");
+          audioDrum.play();
+          break;
+        case "d":
+          audioDrum=new Audio("sounds/tom-4.mp3");
+          audioDrum.play();
+          break;
+        case "j":
+          audioDrum=new Audio("sounds/snare.mp3");
+          audioDrum.play();
+          break;
+        case "k":
+          audioDrum=new Audio("sounds/crash.mp3");
+          audioDrum.play();
+          break;
+        case "l":
+          audioDrum=new Audio("sounds/kick-bass.mp3");
+          audioDrum.play();
+          break;
+          default:alert(this.innerHTML);
+    }
+    });}
+    
+  
+ 
